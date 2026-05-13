104316d49139f97cded637177fe5cbf9b2f5f175
Timmo-G <gdy200212310010@163.com>
2026-05-13 23:58:06 +0800

    modify

---
+++ LeetCode_408(80题)/75. 颜色分类.c
@@ -0,0 +1,16 @@
+void sortColors(int* nums, int numsSize) {
+    int red = 0;
+    int white = 0;
+    int blue = 0;
+    
+    for(int i = 0; i < numsSize;++i){
+        if(nums[i] == 0) red++;
+        if(nums[i] == 1) white++;
+        if(nums[i] == 2) blue++;
+    }
+    
+    while(red != 0 || white != 0 || blue != 0){
+        if()
+    }
+
+}
