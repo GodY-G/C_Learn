9c952201cde0908146e69a45201e11b8d8bdef48
GodY-G <gdy200212310010@163.com>
2026-07-11 16:42:11 +0800

    feat:leetcode20

---
+++ Phase1_C_Fundamentals/20.c
@@ -0,0 +1,19 @@
+#include <stdbool.h>
+bool isValid(char* s) {
+    int len = strlen(s);
+    char stack[len];
+    int top = -1;
+
+    for(int i = 0;i < len;++i){
+        if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
+            stack[++top] = s[i];
+        }else{
+            if(top == -1)return false;
+            char left = stack[top--];
+            if(s[i] == ')' && left != '(')return false;
+            if(s[i] == ']' && left != '[')return false;
+            if(s[i] == '}' && left != '{')return false;
+         }
+    }
+    return top == -1;
+}
